691525dd13c79a7d1cd0c7e6b5fa49da92bc0ba3
Jérémy <3DX900@gmail.com>
2021-10-13 13:19:23 +0200

    ✨ add images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,15 @@
 import React, { FC } from 'react';
 
 const HomePage: FC = () => {
-    return <div>Wsh ça arrive bientôt</div>;
+    return (
+        <>
+            <div>Wsh ça arrive bientôt</div>
+            <img
+                src="/assets/images/menteur.jpeg"
+                alt="Je viens de croiser un menteur là carrément je m'assoie tellement c'est trop"
+            />
+        </>
+    );
 };
 
 export default HomePage;
